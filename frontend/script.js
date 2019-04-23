6542a430a7db080e342398d908a25fe15b3ced8c
Samarpan Harit <haritsamarpan7@gmail.com>
2019-04-23 20:48:45 +0530

    I don't forgot what i was doing

--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,63 @@
 let socket = io(); //creates a socket connection to same server from which server is hosted
-socket.on('connected', () => {
-    console.log("Connected: " + socket.id)
-})
+socket.on("connected", () => {
+    console.log("Connected: " + socket.id);
+});
 $(() => {
-    let sendbtn = $('#send');
+    let sendbtn = $("#send");
     let msglist = document.getElementById("msglist");
-    let msgbox = $('#msgbox');
-    let loginbox = $('#loginbox');
-    let loginbtn = $('#login');
+    let msgbox = $("#msgbox");
+    let loginbox = $("#loginbox");
+    let loginbtn = $("#login");
     let username;
+    let send_event;
+    loginbox.focus();
     loginbtn.click(() => {
         username = loginbox.val();
-        socket.emit('login', username);
-        $('#login-div').css("display", "none");
-        document.title = username
-        $('#chat-div').css("display", "block");
-    })
+        socket.emit("login", username);
+        /*$("#login-div")
+            .css("display", "none")
+            .fadeIn(1000);*/
+        $("#login-div")
+            .hide()
+            .fadeIn(1000);
+        document.title = username;
+        /*$("#chat-div")
+            .css("display", "block")
+            .fadeIn(1000);*/
+        $("#chat-div")
+            .show()
+            .fadeIn(2000);
+    });
+    loginbox.keypress(e => {
+        if (e.charCode === 13) {
+            username = loginbox.val();
+            socket.emit("login", username);
+            $("#login-div").css("display", "none");
+            document.title = username;
+            $("#chat-div").css("display", "block");
+        }
+        msgbox.focus();
+    });
     sendbtn.click(() => {
-        socket.emit('send_msg', {
+        socket.emit("send_msg", {
             username: username,
             message: msgbox.val() //key-value pair
-        })
+        });
         msgbox.val("");
-    })
-    socket.on('received_msg', (msgs) => {
+    });
+    msgbox.keypress(e => {
+        if (e.charCode === 13) {
+            socket.emit("send_msg", {
+                username: username,
+                message: msgbox.val() //key-value pair
+            });
+            msgbox.val("");
+        }
+    });
+    socket.on("received_msg", msgs => {
         console.log(msgs);
-        msglist.innerHTML = msglist.innerHTML + ("<li>" + msgs.username + ": " + msgs.message + "</li>");
-    })
-})
+        msglist.innerHTML =
+            msglist.innerHTML +
+            ("<li>" + msgs.username + ": " + msgs.message + "</li>");
+    });
+});
--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const path = require('path');
-const socketio = require('socket.io');
-const http = require('http');
+const express = require("express");
+const path = require("path");
+const socketio = require("socket.io");
+const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 let usersockets = {};
+//Using middleware
+app.use("/", express.static(path.join(__dirname, "frontend")));
 
-app.use('/', express.static(path.join(__dirname, 'frontend')));
+io.on("connection", socket => {
+  console.log("new socket formed from " + socket.id);
+  socket.emit("connected");
 
-io.on('connection', (socket) => {
-    console.log("new socket formed from " + socket.id)
-    socket.emit('connected');
+  socket.on("login", username => {
+    usersockets[username] = socket.id;
+  });
 
-    socket.on('login', (username) => {
-        usersockets[username] = socket.id;
-    })
-
-    //Msg Received
-    socket.on('send_msg', (msg) => {
-        if (msg.message.startsWith('@')) {
-            let tokens = msg.message.split(':');
-            let recepient = tokens[0].substr(1);
-            msg.message = tokens[1];
-            io.to(usersockets[recepient]).emit('received_msg', msg);
-        } else
-            //sends to all including sender
-            //io.emit('received_msg', msg)
-            //sends to all except sender
-            socket.broadcast.emit('received_msg', msg)
-    })
-})
+  //Msg Received
+  socket.on("send_msg", msg => {
+    if (msg.message.startsWith("@")) {
+      let tokens = msg.message.split(":");
+      let recepient = tokens[0].substr(1);
+      msg.message = tokens[1];
+      io.to(usersockets[recepient]).emit("received_msg", msg);
+    }
+    //sends to all including sender
+    else io.emit("received_msg", msg);
+    //sends to all except sender
+    //socket.broadcast.emit('received_msg', msg)
+  });
+});
 
 server.listen(1738, () => {
-    console.log("Server started at http://localhost:1738");
-})
+  console.log("Server started at http://localhost:1738");
+});
